45dbab4fa3fcb53e80adbeffffd093368c6ea612
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-06-25 18:29:58 -0500

    call method in room_scraper is not working

--- lib/room_scraper.rb
+++ lib/room_scraper.rb
@@ -1,12 +1,34 @@
 class RoomScraper
 
+  @@row = []
+
   def initialize(index_url)
+    @index_url = index_url
     @doc = Nokogiri::HTML(open(index_url))
-    binding.pry
   end
 
-  def rows
-    @rows ||= @doc.search("div.content ul.rows p.result-info")
+
+
+  def call
+    rows.each do |row_doc|
+      data = scrape_row(row_doc)
+      binding.pry
+    end
   end
+
+  private
+    def rows
+      @rows ||= @doc.search("div.content ul.rows p.result-info")
+    end
+
+    def scrape_row(row)
+      # scrape an individual row
+      {
+        :date_created => row.search("datetime"),
+        :title => row.search("a.hdrlink").text,
+        :url => "#{@index_url}#{row.search("a.hdrlink").attribute("href")}",
+        :price => row.search("span.result-price").text,
+      }
+    end
 
 end
