ecb9b2bf36545c65d179ae700d2bf98ba14f7360
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-06-26 22:09:33 -0500

    creating sql databases in Room.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,11 @@ Bundler.require
 
 require 'open-uri'
 
+
+DB = {
+  :connection => SQLite3::Database.new("db/room-developement.sqlite")
+}
+
+#binding.pry
+
 require_all 'lib'
--- lib/room.rb
+++ lib/room.rb
@@ -11,12 +11,29 @@ class Room
     room.date_created = hash[:date_created]
     room.price = hash[:price]
     room.url = hash[:url]
-    binding.pry
+    #binding.pry
     room
   end
 
   def save
     puts "YOU ARE ABOUT TO SAVE #{self}"
+    sql = <<-SQL
+      INSERT INTO rooms (title, date_created, price, url) VALUES (?, ?, ?, ?)
+    SQL
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS rooms (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        date_created DATETIME,
+        price TEXT,
+        url TEXT
+      )
+    SQL
+
+    DB[:connection].execute(sql)
   end
 
 end
