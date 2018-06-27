b986d9248c1a22d8cf173a4925532f60b0c5cabf
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-06-26 21:13:38 -0500

    created self.create_from_hash and self.new_from_hash in Room class

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,21 @@
 class Room
   attr_accessor :title, :date_created, :price, :url
 
+  def self.create_from_hash(hash)
+    new_from_hash(hash).save
+  end
 
+  def self.new_from_hash(hash)
+    room = self.new
+    room.title = hash[:title]
+    room.date_created = hash[:date_created]
+    room.price = hash[:price]
+    room.url = hash[:url]
+    room
+  end
+
+  def save
+    puts "YOU ARE ABOUT TO SAVE #{self}"
+  end
 
 end
--- lib/room_scraper.rb
+++ lib/room_scraper.rb
@@ -1,18 +1,16 @@
 class RoomScraper
 
-  @@row = []
-
   def initialize(index_url)
-    @index_url = index_url
+    @index_url = index_url # need this if i want to use index_url elsewhere
     @doc = Nokogiri::HTML(open(index_url))
   end
 
 
 
   def call
-    rows.each do |row_doc|
-      data = scrape_row(row_doc)
-      binding.pry
+    rows.each do |row_data|
+      Room.create_from_hash(scrape_row(row_data)) #=> should put room in my database
+      #binding.pry
     end
   end
 
@@ -25,10 +23,11 @@ class RoomScraper
       # scrape an individual row
       {
         :date_created => row.search("datetime"),
-        :title => row.search("a.hdrlink").text,
+        :title => row.search("a.hdrlnk").text,
         :url => "#{@index_url}#{row.search("a.hdrlnk").attribute("href").text}",
         :price => row.search("span.result-price").text,
       }
+      #binding.pry
     end
 
 end
